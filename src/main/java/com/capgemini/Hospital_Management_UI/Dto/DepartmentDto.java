7dffbd3eb4030ec094f078fb848f1e80264c6186
Rituraj2610 <26rituraj@gmail.com>
2025-06-13 05:57:15 +0530

    [Rituraj] UPDATE . Resolved errorss

--- src/main/java/com/capgemini/Hospital_Management_UI/Dto/DepartmentDto.java
+++ src/main/java/com/capgemini/Hospital_Management_UI/Dto/DepartmentDto.java
@@ -1,16 +1,37 @@
-package com.capgemini.Hospital_Management_UI.Dto;
+package com.capgemini.Hospital_Management_UI.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 public class DepartmentDto {
-
     private Integer departmentId;
     private String name;
     private PhysicianDepartmentDto physicianDetail;
 
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PhysicianDepartmentDto getPhysicianDetail() {
+        return physicianDetail;
+    }
+
+    public void setPhysicianDetail(PhysicianDepartmentDto physicianDetail) {
+        this.physicianDetail = physicianDetail;
+    }
 }
+
+
+
+
+
